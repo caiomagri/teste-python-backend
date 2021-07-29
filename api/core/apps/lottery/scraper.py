215099ce50b719e814798b6a75f863d5291c2c8f
caiomagri <kaioeduardofm@gmail.com>
2021-07-29 16:12:39 -0300

    [IMP] Validações nas consultas do webscraping

--- api/core/apps/lottery/scraper.py
+++ api/core/apps/lottery/scraper.py
@@ -37,12 +37,12 @@ def get_result_from_cef():
     html = requests.get("http://www.loterias.caixa.gov.br/wps/portal/loterias").content
     soup = BeautifulSoup(html, 'html.parser')
 
-    ul_result = soup.find("ul", attrs={"class": "resultado-loteria mega-sena"})
+    data = soup.find("ul", attrs={"class": "resultado-loteria mega-sena"})
 
-    if not ul_result:
-        pass
+    result_list = []
 
-    result_list = [int(li.text) for li in ul_result.findAll("li")]
+    if data:
+        result_list = [int(li.text) for li in data.findAll("li")]
 
     logger.warning(f"Resultado do webscrapping Caixa Economica Federal - {result_list}")
 
@@ -50,4 +50,7 @@ def get_result_from_cef():
 
 
 def get_lottery_result():
-    return get_result_from_google_search() or get_result_from_cef()
+    response = get_result_from_google_search() or get_result_from_cef()
+    if not response:
+        raise Exception("Não foi possível realizar o webscraping do resultado da mega sena")
+    return response
--- api/core/apps/lottery/views.py
+++ api/core/apps/lottery/views.py
@@ -8,15 +8,19 @@ from rest_framework.response import Response
 from .serializers import BetSerializer
 from .scraper import get_lottery_result
 
-# Get an instance of a logger
 logger = logging.getLogger(__name__)
 
 
 class LotteryResultView(APIView):
 
     def get(self, request, *args, **kwargs):
-        result = get_lottery_result()
-        return Response({"result": result}, status=status.HTTP_200_OK)
+        try:
+            result = get_lottery_result()
+            return Response({"result": result}, status=status.HTTP_200_OK)
+        except Exception as ex:
+            logger.error(f"Get Resultado da Mega Sena - {str(ex)}")
+            return Response({"error": "Tivemos um problema para realizar a requisição."},
+                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 class BetView(APIView):
@@ -29,7 +33,12 @@ class BetView(APIView):
     def post(self, request, *args, **kwargs):
         payload = request.data.copy()
 
-        dozens = int(request.data.get("dozens", 0))
+        try:
+            dozens = int(request.data.get("dozens"))
+        except ValueError as ex:
+            logger.error(f"POST Bet - {str(ex)}")
+            return Response({"erro": "O número de dezenas deve ser do tipo Inteiro."},
+                            status=status.HTTP_400_BAD_REQUEST)
 
         if not (6 <= dozens <= 10):
             return Response({"erro": "O número de dezenas deve ser no mínimo 6 e no máximo 10."},
@@ -53,19 +62,31 @@ class BetView(APIView):
 
 class BetResultView(APIView):
     def get(self, request, *args, **kwargs):
-        bet = request.user.bets.all().first()
-        serializer = BetSerializer(bet)
+        try:
 
-        result = get_lottery_result()
-        numbers = serializer.data.get("numbers")
-        right_numbers = list(set(numbers) & set(result))
+            bet = request.user.bets.all().first()
 
-        response = {
-            "right_numbers": {
-                "total_right": len(right_numbers),
-                "numbers": right_numbers
-            },
-            "numbers": numbers,
-            "result": result
-        }
-        return Response(response)
+            if not bet:
+                return Response({"erro": "Você ainda não fez nenhuma aposta."},
+                                status=status.HTTP_400_BAD_REQUEST)
+
+            serializer = BetSerializer(bet)
+
+            result = get_lottery_result()
+            numbers = serializer.data.get("numbers")
+            right_numbers = sorted(list(set(numbers) & set(result)))
+
+            response = {
+                "right_numbers": {
+                    "total_right": len(right_numbers),
+                    "numbers": right_numbers
+                },
+                "numbers": numbers,
+                "result": result
+            }
+
+            return Response(response)
+        except Exception as ex:
+            logger.error(f"Get Resultado da Última Aposta - {str(ex)}")
+            return Response({"error": "Tivemos um problema para realizar a requisição."},
+                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
