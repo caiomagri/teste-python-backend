0d09d3f1a552f11ce171246a5e953b4e9c8feeb2
caiomagri <kaioeduardofm@gmail.com>
2021-07-29 15:32:02 -0300

    [ADD] duas formas de buscar resultados da mega sena

--- api/core/apps/lottery/scraper.py
+++ api/core/apps/lottery/scraper.py
@@ -1,9 +1,39 @@
+import urllib
+import logging
 import requests
 
 from bs4 import BeautifulSoup
 
+logger = logging.getLogger(__name__)
 
-def get_lottery_result():
+
+def get_result_from_google_search():
+    url = 'https://www.google.com/search?q=caixa+mega+sena'
+
+    request = urllib.request.Request(url)
+
+    request.add_header('User-Agent',
+                       'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 '
+                       '(KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36')
+
+    raw_response = urllib.request.urlopen(request).read()
+
+    html = raw_response.decode("utf-8")
+    soup = BeautifulSoup(html, 'html.parser')
+
+    data = soup.findAll("span", attrs={"class": "zSMazd"})
+
+    result_list = []
+
+    if data:
+        result_list = [int(span.text) for span in data]
+
+    logger.warning(f"Resultado do webscrapping Google - {result_list}")
+
+    return result_list
+
+
+def get_result_from_cef():
     html = requests.get("http://www.loterias.caixa.gov.br/wps/portal/loterias").content
     soup = BeautifulSoup(html, 'html.parser')
 
@@ -14,4 +44,10 @@ def get_lottery_result():
 
     result_list = [int(li.text) for li in ul_result.findAll("li")]
 
+    logger.warning(f"Resultado do webscrapping Caixa Economica Federal - {result_list}")
+
     return result_list
+
+
+def get_lottery_result():
+    return get_result_from_google_search() or get_result_from_cef()
